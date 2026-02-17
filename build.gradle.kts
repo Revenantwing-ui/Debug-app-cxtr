d41f9df56008424bb237296ebfc01cd6c4efe220
Julian Lyman <gumtrident714@gmail.com>
2026-02-17 13:57:22 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,7 @@
-// ROOT build.gradle.kts
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        // Defines the plugin versions for the whole project
-        classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-    }
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    // THIS FIXES THE "PLUGIN NOT FOUND" ERROR:
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
-
-// Do NOT include 'allprojects' or 'plugins' blocks here.
-// Repositories are handled in settings.gradle.kts
